7ef85eb29b62c532138dab05935ba662b2e335b3
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-04-08 22:53:04 -0500

    fix(chore): delete unused table

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -70,16 +70,4 @@ CREATE TABLE TASK_TAGS
             ON DELETE CASCADE
 );
 
---
-CREATE TABLE event_publication
-(
-    id               UUID NOT NULL PRIMARY KEY,
-    listener_id      VARCHAR(512) NOT NULL,
-    event_type       VARCHAR(512) NOT NULL,
-    serialized_event VARCHAR(4000) NOT NULL,
-    publication_date TIMESTAMP WITH TIME ZONE NOT NULL,
-    completion_date  TIMESTAMP WITH TIME ZONE,
-    completion_attempts INTEGER
-);
-
 END;
